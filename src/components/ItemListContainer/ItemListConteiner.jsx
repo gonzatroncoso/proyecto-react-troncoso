87d827ac1e5730ca921d7c447488a4c411fdd03d
Gonzalo Troncoso <gonzalo.troncoso10@hotmail.com>
2021-11-02 20:26:16 -0300

    contador de productos

--- src/components/ItemListContainer/ItemListConteiner.jsx
+++ src/components/ItemListContainer/ItemListConteiner.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 
 const  ItemListContainer = ({saludo}) => {
     return (
        <>
         <h1>{saludo}</h1>
+        <ItemCount  initial = {0} stock={10} />
        </>
    );
   }
